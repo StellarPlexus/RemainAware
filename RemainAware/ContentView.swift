568b432a9ccdd502d3e396df89b2f710e8ff28e4
Stellarplexus <Stellarplexuses@gmai.com>
2025-02-12 21:11:46 -0600

    Initial Commit

---
+++ RemainAware/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  RemainAware
+//
+//  Created by Michael Killebrew on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.purple)
+            Text("Remain Aware!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+        }
+        // New Stuff Here
+        .padding()
+        
+    }
+} 
+
+#Preview {
+    ContentView()
+}
---
+++ RemainAware/RemainAwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemainAwareApp.swift
+//  RemainAware
+//
+//  Created by Michael Killebrew on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RemainAwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
